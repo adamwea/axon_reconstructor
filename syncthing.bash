825d2a1fcf67accec8e352d1ca9e3140a81ff82c
adamwea <amwe@ucdavis.edu>
2024-11-05 16:56:44 -0800

    implementing analysis

--- syncthing.bash
+++ syncthing.bash
@@ -1,2 +1,2 @@
 nohup syncthing &
-nohup python3 your_script.py &
+nohup python3 /home/adamm/workspace/RBS_axonal_reconstructions/modules/analyze_and_reconstruct/analyze.py >> /home/adamm/workspace/RBS_axonal_reconstructions/logs/analyze.log 2>&1 &
