e4e9aaf6f26baafce551a5db5dc1aaed71d4755c
adamm_NERSC <adammwea@login06.chn.perlmutter.nersc.gov>
2024-11-20 11:55:24 -0800

    sync

--- sbatch_scripts/parallel_pipeline_execution_Human_E_Neurons_T1_06072024.sh
+++ sbatch_scripts/parallel_pipeline_execution_Human_E_Neurons_T1_06072024.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=pipeline_test
+#SBATCH --job-name=Human_E_Neurons_T1_06072024
 #SBATCH -A m2043_g
-#SBATCH -t 00:30:00  # Time limit in the form hh:mm:ss
-#SBATCH -N 2  # Number of nodes requested, adjust as needed
+#SBATCH -t 06:00:00  # Time limit in the form hh:mm:ss
+#SBATCH -N 6  # Number of nodes requested, adjust as needed
 #SBATCH --mail-user=amwe@ucdavis.edu
 #SBATCH --mail-type=ALL
-#SBATCH -q debug
+#SBATCH -q regular
 #SBATCH -C gpu
-#SBATCH --gpus=2
-#SBATCH --output=parallel_pipeline_test_%j.out
-#SBATCH --error=parallel_pipeline_test_%j.err
+#SBATCH --gpus=6
+#SBATCH --output=Human_E_Neurons_T1_06072024_%j.out
+#SBATCH --error=Human_E_Neurons_T1_06072024_%j.err
 
 # Functions ====================================================================
 # Function to generate the list of wells (HDF5 files) to process
@@ -53,16 +53,17 @@ generate_log_file_names() {
 # Args ========================================================================
 # Define the HDF5 directories containing wells
 H5_PARENT_DIRS=(
-    "/pscratch/sd/a/adammwea/RBS_synology_rsync/B6J_DensityTest_10012024_AR"
+    "/pscratch/sd/a/adammwea/xRBS_input_data/Human_E_Neurons_T1_06072024"
 )
 
 # Define output and log directories
 export OUTPUT_DIR="/pscratch/sd/a/adammwea/zRBS_axon_reconstruction_output"
-LOG_DIR="/pscratch/sd/a/adammwea/zRBS_axon_reconstruction_output/logs"
+export LOG_DIR="/pscratch/sd/a/adammwea/zRBS_axon_reconstruction_output/logs"
+export ANALYZED_FILE="${OUTPUT_DIR}/analyzed_plate_files.txt"
 mkdir -p ${OUTPUT_DIR} ${LOG_DIR}
 
 # Define the maximum number of parallel jobs
-MAX_JOBS=2  # Adjust based on your resources
+MAX_JOBS=6  # Adjust based on your resources
 
 # Define the full path to the Python script
 PYTHON_SCRIPT_PATH="/pscratch/sd/a/adammwea/RBS_axonal_reconstructions/development_scripts/developing_nbs_for_AR_density_projects/run_pipeline_HPC.py"
@@ -83,15 +84,29 @@ generate_plate_files
 echo "Number of plates to process: ${#PLATE_FILES[@]}"
 echo "Number of streams per plate: 6"
 echo "Total number of tasks: $(( ${#PLATE_FILES[@]} * 6 ))"
+echo "Maximum number of parallel jobs: $MAX_JOBS"
+
+# Load analyzed plate files if the file exists
+declare -A ANALYZED_PLATE_FILES
+if [[ -f "$ANALYZED_FILE" ]]; then
+    while IFS= read -r line; do
+        ANALYZED_PLATE_FILES["$line"]=1
+    done < "$ANALYZED_FILE"
+fi
 
 # Prepare input file for GNU Parallel
 input_file="parallel_input.txt"
 > $input_file
 for plate_file in "${PLATE_FILES[@]}"; do
     for stream in $(seq 0 5); do
-        echo "$plate_file $stream" >> $input_file
+        pair="${plate_file} ${stream}"
+        if [[ -z "${ANALYZED_PLATE_FILES[$pair]}" ]]; then
+            echo "$pair" >> $input_file
+        fi
     done
 done
+# Reverse the order of tasks in the input file
+tac $input_file > temp_file && mv temp_file $input_file
 
 # Function to process each task
 process_task() {
@@ -103,6 +118,7 @@ process_task() {
     srun -n 1 --gres=gpu:1 shifter --image=$SHIFTER_IMAGE python3 $PYTHON_SCRIPT_PATH \
         --plate_file "$plate_file" --stream_select "$stream" --output_dir "$OUTPUT_DIR" \
         > "$log_file" 2> "$error_log_file"
+    echo "$plate_file $stream" >> "$ANALYZED_FILE"
 }
 
 export -f process_task
